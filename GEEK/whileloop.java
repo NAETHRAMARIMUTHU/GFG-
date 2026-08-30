26a95babdc7c409d09d6e0212a26a95531cb5b6c
NAETHRAMARIMUTHU <naethra1111@gmail.com>
2026-08-30 17:25:11 +0530

    Create whileloop.java

---
+++ GEEK/whileloop.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void utility(int x) {
+        // code here
+        while(x!=-1){
+            System.out.print(x+" ");
+            
+            x--;
+        }
+    }
+}
