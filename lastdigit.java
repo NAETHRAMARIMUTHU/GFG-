b7ea55d201407217282eb75b382dcbd666f612f0
NAETHRAMARIMUTHU <naethra1111@gmail.com>
2026-08-31 12:24:22 +0530

    Create lastdigit.java

---
+++ lastdigit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        // code here
+        int res=n%10;
+        System.out.print(Math.abs(res));
+    }
+}
